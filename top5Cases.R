94d43fadb0a424e78d9a7673d2552ce3771e70ef
DataScienceProjectsJapan <wclaster@apu.ac.jp>
2021-04-07 18:23:26 +0900

    5 prefectures

--- top5Cases.R
+++ top5Cases.R
@@ -20,7 +20,9 @@ totalPos$Posi
 #|prefectureNameE=="Osaka"|prefectureNameE=="Fukuoka"|prefectureNameE=="Hokaido"
 TokyoCases <- prefectures  %>% filter(prefectureNameE=="Tokyo")%>% select("testedPositive", "year","month","date")  
 top5=c("Tokyo","Osaka","Fukuoka","Sendai","Hokkaido")
-top5Cases <- prefectures  %>% filter(prefectureNameE %in% top5)%>% select("testedPositive", "year","month","date")  
+top5Cases <- prefectures%>%group_by(prefectureNameE)   %>%  filter(prefectureNameE %in% top5)%>%ungroup() %>% select("prefectureNameE","testedPositive", "year","month","date") 
+glimpse(top5Cases)
+View(top5Cases)
 View(TokyoCases)
 str(TokyoCases)
 str(as.vector(TokyoCases))
@@ -28,9 +30,18 @@ str(as.vector(TokyoCases))
 TokyoCases["testedPositive"]
 #or
 TokyoCases$testedPositive
-TokyoCasesDaily <- TokyoCases$testedPositive[2:length(TokyoCases$testedPositive)]-TokyoCases$testedPositive[1:(length(TokyoCases$testedPositive)-1)]
-TokyoCasesDaily
+top5Cases$Date <- as.Date(paste(top5Cases$year,top5Cases$month,top5Cases$date,sep = "-"))
 
+View(top5Cases)
+top5Casesgrouped <-  top5Cases %>% group_by(prefectureNameE) %>% select(prefectureNameE,testedPositive,Date) %>% arrange(prefectureNameE,Date)
+
+View(top5Casesgrouped)
+
+TokyoCasesDaily <- TokyoCases$testedPositive[2:length(TokyoCases$testedPositive)]-TokyoCases$testedPositive[1:(length(TokyoCases$testedPositive)-1)]
+top5newcases <- top5Casesgrouped$testedPositive[2:length(top5Casesgrouped$testedPositive)]-top5Casesgrouped$testedPositive[1:(length(top5Casesgrouped$testedPositive)-1)]
+glimpse(top5newcases)
+top5Casesgrouped$newCases <- c(top5newcases,0)
+View(top5Casesgrouped)
 #Graphs
 ggplot(as.data.frame(TokyoCasesDaily), aes(x=1:length(TokyoCasesDaily),y=TokyoCasesDaily))+geom_line()
 #convert to date.
@@ -65,3 +76,10 @@ View(TokyoCases)
 
 
 ggplot(TokyoCases, aes(x = Date, y=TokyoCasesDaily)) + geom_point() + geom_smooth(span = 0.2)+labs(title = "Tokyo daily new cases", x = "", y = "New cases",subtitle = "7-day smoothing in blue")
+
+library(viridis)
+#See some color options here
+#https://www.r-graph-gallery.com/ggplot2-color.html
+
+ggplot(top5Casesgrouped, aes(x = Date, y=newCases, colour = prefectureNameE)) + geom_point(size=.1)+ geom_smooth(aes(group = prefectureNameE),span = 0.2) +labs(title = "Japan 5 Cities", x = "", y = "New cases",subtitle = "7-day smoothing")+ylim(0,2000) +scale_color_manual(values=c("#69b3a2", "purple", "black","brown2","blue1"))
+  #scale_color_brewer(palette = "Paired") #scale_color_viridis(discrete=TRUE, option="plasma")
